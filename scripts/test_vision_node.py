c17daf563bc9bcc844bd632449fadded1d1e4156
Farhan <farhan_bd9@yahoo.com>
2019-05-02 13:45:03 +0800

    Problem with script

---
+++ scripts/test_vision_node.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+import rospy
+import sys
+import cv2
+from std_msgs.msg import String
+from sensor_msgs.msg import CompressedImage
+from cv_bridge import CvBridge, CvBridgeError
+
+class TestVisionNode :
+    def __init__(self):
+        #initialize node (name)
+        rospy.init_node("test_vision_node")
+        #create the cvbridge object
+        self.bridge = CvBridge()
+        #subscribe to raw image/compressed topic
+        self.sub = rospy.Subscriber("image_raw/compressed", CompressedImage, self.callback)
+
+    def callback(self, data):
+        try:
+            #convert the raw to opencv
+            cv_image = self.bridge.compressed_imgmsg_to_cv2(data)
+        except CvBridgeError as e:
+                print (e)
+    #display image
+        cv2.imshow("image",cv_image)
+        cv2.waitkey(1) #refresh every second
+    def main(args):
+        vn=TestVisionNode()
+        try:
+            rospy.spin()
+        except KeyboardInterrupt:
+            print("Node Shutdown")
+            cv2.closeAllWindows()
+    if __name__ == "__main__":
+        main(sys.argv)
